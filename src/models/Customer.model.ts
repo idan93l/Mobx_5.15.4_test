314044e276bfd43739694c91fb0211d33f0c92d5
Idan Lev <idan.93lev@gmail.com>
2023-01-26 23:14:25 +0200

    customer store 1

--- src/models/Customer.model.ts
+++ src/models/Customer.model.ts
@@ -1,7 +1,3 @@
-import { observable } from "mobx"
-
 export default class CustomerModel {
-  id = Math.random();
-  @observable name = "";
-  @observable age = "";
+  constructor() {}
 }
--- src/stores/CurrentCustomer.store.ts
+++ src/stores/CurrentCustomer.store.ts
@@ -1,7 +1,18 @@
 import CustomerModel from "../models/Customer.model";
+import { observable, action } from "mobx";
+
+interface Customer {
+  id: number;
+  name: string;
+  age: number;
+  email: string;
+}
 
 export default class CurrentCustomerStore {
-  // currentCustomer: CustomerModel;
+  
+  customers: Customer[] = [];
 
-  constructor() {}
+  constructor() {
+    makeObservable()
+  }
 }
