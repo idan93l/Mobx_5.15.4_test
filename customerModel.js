ca165de8b0e6606c82e815afe849aeeead531243
Idan Lev <idan.93lev@gmail.com>
2023-01-29 02:42:14 +0200

    mongoose utils added

--- customerModel.js
+++ customerModel.js
@@ -1,9 +1,13 @@
-// const mongoose = require("mongoose");
-// const Schema = mongoose.Schema;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-// const CustomerSchema = new Schema({
-// });
+const CustomerSchema = new Schema({
+  name: string,
+  age: number
+}, {timestamps: true});
 
-// module.exports = mongoose.model);
+const CustomerModel = mongoose.model("Customers", CustomerSchema)
+
+module.exports = CustomerModel;
 
 // Please comment out all your code when you are finished.
--- updateCurrentUser.js
+++ updateCurrentUser.js
@@ -1,5 +1,27 @@
-// updateCurrentCustomer = (currentCustomerID) => {
-//     try {
-//   };
+const CustomerModel = require("./customerModel");
+
+updateCurrentCustomer = async (req, res) => {
+  const { currentCustomerID } = req.params;
+  const { name, age } = req.body;
+  try {
+    const customer = await CustomerModel.findOne({ _id: currentCustomerID });
+    if (!customer) {
+      const newCustomer = await new CustomerModel(
+        { _id: currentCustomerID },
+        { name: name },
+        { age: age }
+      );
+      await newCustomer.save();
+    }
+    const UpdatedCustomer = await CustomerModel.findOneAndUpdate(
+      { _id: currentCustomerID },
+      { name: name },
+      { age: age }
+    );
+    res.status(200).send(UpdatedCustomer);
+  } catch (err) {
+    res.status(400).send({ error: err.message });
+  }
+};
 
 // Please comment out all your code when you are finished.
